527880f61a7a1377c9d8c9be7d9a9c56bf772715
Brian Gibson <gibsonbg@me.com>
2022-05-07 11:04:34 -0400

    Adds imports to file; Test suite failed to run

--- src/x-402400-sn-jest-test/__tests__/test.x-402400-sn-jest-test.js
+++ src/x-402400-sn-jest-test/__tests__/test.x-402400-sn-jest-test.js
@@ -1,3 +1,8 @@
+import Enzyme from 'enzyme';
+import {UIEnzymeAdapter} from '@servicenow/ui-enzyme-adapter';
+
+Enzyme.configure({adapter: new UIEnzymeAdapter()});
+
 describe('x-402400-sn-jest-test Test', () => {
 	it('should be true', () => {
 		expect(true).toBe(true);
